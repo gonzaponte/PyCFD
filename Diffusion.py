3b0c93f9142cb112577b0071815dacd873b34caf
gonzaponte <gonzaponte@gmail.com>
2015-08-01 20:51:43 +0200

    Added comparison with true

--- Diffusion.py
+++ Diffusion.py
@@ -9,48 +9,60 @@ from math import *
 
 ### SIMULATION VARIABLES
 
-dx = 1e-1
-dt = 5e-1
+dx = 2e-2
+dt = 1e-1
 
 t0 = 0.
-t1 = 500.
+t1 = 50.
 
 x0 = 0.
-x1 = 4.
+x1 = 1.
 
 nx = int( (x1-x0) / dx )
 nt = int( (t1-t0) / dt )
 
-alpha = 1e-2
+alpha = 1e-4
 s1  = alpha * dt / dx**2
 s2 = 1 - 2*s1 
 
 assert s1 < 1.,'Nope. S1 = '+ str(s1)
 
 data = np.zeros( (nt,nx) )
+true = np.zeros( (nt,nx) )
 
 ###### BOUNDARY CONDITIONS
 T0 = 100.
 
-data[0,:] = 0.
+data[0,:]  = true[0,:] = 0.
 
-data[:,0] = T0
-data[:,-1] = T0
+data[:,0]  = true[:,0] = T0
+data[:,-1] = true[:,-1] = T0
 
 ### SIMULATION
 for t in range(nt-1):
+    tr = (t+1) * dt 
     for x in range(1,nx-1):
+        xn = x / ( nx - 1 ) 
         data[t+1,x] = s2 * data[t,x] + s1 * ( data[t,x-1] + data[t,x+1] )
+        true[t+1,x] = 100 - sum( 400 / pi / ( 2*m+1 ) * sin( (2*m+1)*pi*xn ) * exp(-alpha*pi**2*tr*(2*m+1)**2) for m in range(20) )
 
 ### PLOTTING
 cv = rt.TCanvas()
-hs = [ rt.TH1F( str(i),';x;T', nx, x0, x1 ) for i in range(nt) ]
-for h in hs: h.SetMinimum(0.)
-for h in hs: h.SetMaximum(1.1*T0)
+hdata = [ rt.TH1F( 'data_' + str(i),';x;T', nx, x0, x1 ) for i in range(nt) ]
+htrue = [ rt.TH1F( 'true_' + str(i),';x;T', nx, x0, x1 ) for i in range(nt) ]
+
+for h in hdata: h.SetMinimum(0.)
+for h in hdata: h.SetMaximum(1.1*T0)
+for h in htrue: h.SetMinimum(0.)
+for h in htrue: h.SetMaximum(1.1*T0)
+for h in htrue: h.SetLineColor(rt.kRed)
+
 for t in range(nt):
     for x in range(nx):
-        hs[t].SetBinContent(x+1,data[t,x])
-    hs[t].Draw()
+        hdata[t].SetBinContent(x+1,data[t,x])
+        htrue[t].SetBinContent(x+1,true[t,x])
+    hdata[t].Draw()
+    htrue[t].Draw('same')
     cv.Update()
 #    raw_input()
     tm.sleep(0.05)
