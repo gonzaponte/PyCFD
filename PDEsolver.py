ef13656c6cf72090cf46e0c67901fb1e00965e17
Gonzalo <gonzaponte@gmail.com>
2016-04-17 21:12:24 +0200

    pdesolver

---
+++ PDEsolver.py
@@ -0,0 +1,17 @@
+'''
+    PDEsolver contains the appropiate functions and classes
+    needed to solve partial differential equations.
+'''
+
+from __future__ import *
+from math import *
+
+class PDEsolver:
+    '''
+        Solves partial differential equations.
+    '''
+    def __init__( self, data,  ):
+        self.data = data
+
+    def FCTS( self ):
+        pass
